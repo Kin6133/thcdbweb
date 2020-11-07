315244b3ac7f2b5886dbe993423ac7d971fe1fbd
Kin6133 <Kin6133>
2020-11-07 14:58:16 +0700

    Login

--- Scripts/login.js
+++ Scripts/login.js
@@ -10,3 +10,34 @@ function show()
         pw.type = "password";
     }
 }
+function enter()
+{
+    var us = document.getElementById("umail");
+    var pw = document.getElementById("pw");
+    var form = document.getElementById("form");
+    var html = document.getElementById("html");
+    if (us.value == "KinCubing" || us.value == "phakhinthon@gmail.com" || us.value == "2018PRON02")
+    {
+        if (pw.value == "KinCubingAdmin30042549" && html.lang == "en")
+        {
+            us.name = "KinCubing";
+            form.action = "welcome-en.php";
+        }
+        else if (pw.value == "KinCubingAdmin30042549" && html.lang == "th")
+        {
+            form.action = "welcome-th.php";
+        }
+        else if (pw.value == "KinCubingAdmin30042549" && html.lang == "jp")
+        {
+            form.action = "welcome-jp.php";
+        }
+        else
+        {
+            alert("Wrong password or username.");
+        }
+    }
+    else
+    {
+        alert("Wrong password or username.");
+    }
+}
---
+++ welcome-en.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <h1>WCA ID : <?php echo $_POST["id"]; ?></h1><br>
+    </body>
+</html>
