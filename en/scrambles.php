9174543dacb644a0a3ebd58e24e4d93b4da53f51
Kin6133 <55972304+Kin6133@users.noreply.github.com>
2020-11-08 20:31:02 +0700

    Scrambles Added : FMC Asia 2019 Fixed

--- en/scrambles.php
+++ en/scrambles.php
@@ -3,7 +3,7 @@
     <?php include "headscr.html"; ?>
     <h1>Note : Events are ordered by competitions' schedules.</h1>
     <h1><a href="scrambles.php?compid=THC2014">Scrambles for Thailand Championship 2014</a></h1>
-    <h1><a href="scrambles.php?compid-FMCASIA2019">Scrambles for FMC Asia 2019</a></h1>
+    <h1><a href="scrambles.php?compid=FMCASIA2019">Scrambles for FMC Asia 2019</a></h1>
     <?php 
         $gotcomp = $_GET["compid"];
         if ($gotcomp == "THC2014") 
