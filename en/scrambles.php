3e7d90173fb950b4d28b29577035e4919d08f682
Kin6133 <55972304+Kin6133@users.noreply.github.com>
2020-11-08 19:44:12 +0700

    THC2014 Fixed V69420

--- en/scrambles.php
+++ en/scrambles.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <?php include "headscr.html"; ?>
-    <h1><a href="scrambles.php?compid=THC2014">Scrambles for Thailand Championship 2014</a></h1>
+    <h1><a href="scrambles.php?compid=THC2014" <?php $compid == "THC2014"; ?>>Scrambles for Thailand Championship 2014</a></h1>
     <?php 
         if ($compid == "THC2014") 
         {
