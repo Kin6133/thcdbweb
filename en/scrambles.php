d0e95a03a84c479fa05369b6ab07830c6dc4e14c
Kin6133 <55972304+Kin6133@users.noreply.github.com>
2020-11-08 21:06:56 +0700

    More Added

--- en/scrambles.php
+++ en/scrambles.php
@@ -2,8 +2,9 @@
 <html>
     <?php include "headscr.html"; ?>
     <h1>Note : Events are ordered by competitions' schedules.</h1>
-    <h1><a href="scrambles.php?compid=THC2014">Scrambles for Thailand Championship 2014</a></h1>
     <h1><a href="scrambles.php?compid=FMCASIA2019">Scrambles for FMC Asia 2019</a></h1>
+    <h1><a href="scrambles.php?compid=FMCASIA2018">Scrambles for FMC Asia 2018</a></h1>
+    <h1><a href="scrambles.php?compid=THC2014">Scrambles for Thailand Championship 2014</a></h1>
     <?php 
         $gotcomp = $_GET["compid"];
         if ($gotcomp == "THC2014") 
@@ -13,6 +14,10 @@
         elseif ($gotcomp == "FMCASIA2019")
         {
             include("fmcasia2019.html");
+        }
+        elseif ($gotcomp == "FMCASIA2018")
+        {
+            include("fmcasia2018.html");
         }
     ?>
 </html>
