cea418904f54d0b6d554333bb56a5cda926d403b
Kin6133 <55972304+Kin6133@users.noreply.github.com>
2020-11-08 19:12:47 +0700

    THC2014

--- en/scrambles.php
+++ en/scrambles.php
@@ -1,1 +1,6 @@
-<?php include "headscr.html"; ?>
+<!DOCTYPE html>
+<html>
+    <?php include "headscr.html"; ?>
+    <a href="scrambles.php?compid=THC2014">Scrambles for Thailand Championship 2014</a>
+    <?php if ($compid == "THC2014") {include "thc2014.html";}?>
+</html>
