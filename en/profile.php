e254b541a79f3de4d75ea2680edc8b15094da37e
Kin6133 <Kin6133>
2020-11-14 22:08:35 +0700

    User ID

--- en/profile.php
+++ en/profile.php
@@ -3,6 +3,6 @@ $user = $_GET["username"];
 $pw = $_GET["password"];
 if ($user == "KinCubing" && $pw == "MzAwNEtpbkN1YmluZw==")
 {
-    echo "Username : KinCubing";
+    include "User DB/id001am.html";
 }
 ?>
