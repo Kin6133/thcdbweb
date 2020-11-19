15ae23c072b75650672d3e7b032d6c46b29f87f9
Kin6133 <Kin6133>
2020-11-19 18:33:35 +0700

    NR Scrambles

--- en/scrambles.php
+++ en/scrambles.php
@@ -6,6 +6,7 @@
             <form action="scrambles.php">
                 <select name="compid">
                     <option>-Select the competition-</option>
+                    <option value="NRSINGLE">All NR Single Scrambles</option>
                     <option value="FMCASIA2019">FMC Asia 2019</option>
                     <option value="FMCASIA2018">FMC Asia 2018</option>
                     <option value="THC2014">Thailand Championship 2014</option>
@@ -19,13 +20,17 @@
         {
            include("thc2014.html");
         }
+        elseif ($gotcomp == "FMCASIA2018")
+        {
+            include("fmcasia2018.html");
+        }
         elseif ($gotcomp == "FMCASIA2019")
         {
             include("fmcasia2019.html");
         }
-        elseif ($gotcomp == "FMCASIA2018")
+        elseif ($gotcomp == "NRSINGLE")
         {
-            include("fmcasia2018.html");
+            include("nrsingle.html");
         }
     ?>
 </html>
