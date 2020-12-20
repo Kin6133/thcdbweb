1677115103a0dfda79dd74d03234ac38fdf6fe43
Kin6133 <55972304+Kin6133@users.noreply.github.com>
2020-12-20 22:26:38 +0700

    News Edited, PHP Fixed

--- en/index.php
+++ en/index.php
@@ -1,13 +1,15 @@
 <?php 
-    include("index.html");
     $newsid = $_GET["news"];
+    include("index.html");
+    include("002ctx.html");
+    if ($newsid == "002")
+    {
+        include("News/002.html");
+    }
+    include("001ctx.html");
     if ($newsid == "001")
     {
         include("News/001.html");
-    }
-    else if ($newsid == "002")
-    {
-        include("News/002.html");
     }
     include("comptable.html");
 ?>
