bbe79af79424dff278ab272b11c8c2041f120a37
thcdb <phakhinthon@gmail.com>
2021-01-01 22:25:44 +0700

    Worlds News

--- en/index.php
+++ en/index.php
@@ -1,6 +1,11 @@
 <?php 
     $newsid = $_GET["news"];
     include("index.html");
+    include("003ctx.html");
+    if ($newsid == "003")
+    {
+        include("News/003.html");
+    }
     include("002ctx.html");
     if ($newsid == "002")
     {
